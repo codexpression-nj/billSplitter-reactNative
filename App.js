5875474404e67a386ec8fa9e4780091785cefd9c
codexpression-nj <n.codexpression@gmail.com>
2023-02-10 23:33:23 +0200

    update

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NewBill from './components/NewBill';
 import SignUp from './components/SignUp';
+import SignIn from './components/Signin';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
     <Stack.Navigator styles={{backgroundColor:'white'}} initialRouteName="signup">
       <Stack.Screen name="NewBill" component={NewBill} options={{headerShown:false}}/>
       <Stack.Screen name="signup" component={SignUp} options={{headerShown:false}}/>
+      <Stack.Screen name="signIn" component={SignIn} options={{headerShown:false}}/>
     </Stack.Navigator>
   </NavigationContainer>
   );
